4295f8585030675c69c7086835346064137e112a
gabriel <gabrielalmeidads@gmail.com>
2022-01-20 22:03:57 -0300

    Updating name for app class in main

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,17 @@ import 'package:finhelper/src/app_widget.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(AppFirebase());
+  runApp(AppFinHelper());
 }
 
-class AppFirebase extends StatefulWidget {
-  const AppFirebase({Key? key}) : super(key: key);
+class AppFinHelper extends StatefulWidget {
+  const AppFinHelper({Key? key}) : super(key: key);
 
   @override
-  _AppFirebaseState createState() => _AppFirebaseState();
+  _AppFinHelperState createState() => _AppFinHelperState();
 }
 
-class _AppFirebaseState extends State<AppFirebase> {
+class _AppFinHelperState extends State<AppFinHelper> {
   @override
   Widget build(BuildContext context) {
     return AppWidget();
