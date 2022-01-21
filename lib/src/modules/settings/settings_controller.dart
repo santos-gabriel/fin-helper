f3034f79e1a069d57a9307270854ad86bdbc4161
gabriel <gabrielalmeidads@gmail.com>
2022-01-20 21:56:34 -0300

    Updating settings

--- lib/src/modules/settings/settings_controller.dart
+++ lib/src/modules/settings/settings_controller.dart
@@ -1,73 +1,62 @@
 import 'package:finhelper/src/shared/models/settings_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsController {
   SettingsModel? _settingsModel;
 
   SettingsModel get settings => _settingsModel!;
 
-  bool allowNotifications() {
-    if (_settingsModel == null) {
-      _settingsModel = SettingsModel(
-        allowNotifications: false,
-        allowPasswordOnLogin: false,
-        rememberLogin: false,
-      );
+  Future<void> saveSettings(SettingsModel settings) async {
+    final instance = await SharedPreferences.getInstance();
+    await instance.setString("settings", settings.toJson());
+  }
+
+  setSettings(SettingsModel settings) {
+    saveSettings(settings);
+    _settingsModel = settings;
+  }
+
+  Future<SettingsModel?> currentSettings() async {
+    final instance = await SharedPreferences.getInstance();
+    // await Future.delayed(Duration(seconds: 2));
+    if (instance.containsKey("settings")) {
+      final json = instance.get("settings") as String;
+      setSettings(SettingsModel.fromJson(json));
+      return this.settings;
+    } else {
+      SettingsModel settings = SettingsModel(
+          allowNotifications: false,
+          allowPasswordOnLogin: false,
+          rememberLogin: false);
+      setSettings(settings);
+      _settingsModel = settings;
     }
+  }
+
+  bool allowNotifications() {
     return _settingsModel!.allowNotifications;
   }
 
   bool allowPasswordOnLogin() {
-    if (_settingsModel == null) {
-      _settingsModel = SettingsModel(
-        allowNotifications: false,
-        allowPasswordOnLogin: false,
-        rememberLogin: false,
-      );
-    }
     return _settingsModel!.allowPasswordOnLogin;
   }
 
   bool rememberLogin() {
-    if (_settingsModel == null) {
-      _settingsModel = SettingsModel(
-        allowNotifications: false,
-        allowPasswordOnLogin: false,
-        rememberLogin: false,
-      );
-    }
     return _settingsModel!.rememberLogin;
   }
 
   void setAllowNotifications(bool value) {
-    if (_settingsModel == null) {
-      _settingsModel = SettingsModel(
-        allowNotifications: false,
-        allowPasswordOnLogin: false,
-        rememberLogin: false,
-      );
-    }
     _settingsModel!.allowNotifications = value;
+    saveSettings(settings);
   }
 
   void setAllowPasswordOnLogin(bool value) {
-    if (_settingsModel == null) {
-      _settingsModel = SettingsModel(
-        allowNotifications: false,
-        allowPasswordOnLogin: false,
-        rememberLogin: false,
-      );
-    }
     _settingsModel!.allowPasswordOnLogin = value;
+    saveSettings(settings);
   }
 
   void setRememberLogin(bool value) {
-    if (_settingsModel == null) {
-      _settingsModel = SettingsModel(
-        allowNotifications: false,
-        allowPasswordOnLogin: false,
-        rememberLogin: false,
-      );
-    }
     _settingsModel!.rememberLogin = value;
+    saveSettings(settings);
   }
 }
--- lib/src/modules/settings/settings_page.dart
+++ lib/src/modules/settings/settings_page.dart
@@ -1,6 +1,5 @@
 import 'package:finhelper/src/modules/settings/settings_controller.dart';
 import 'package:finhelper/src/shared/auth/auth_controller.dart';
-import 'package:finhelper/src/shared/database/DBProvider.dart';
 import 'package:finhelper/src/shared/themes/app_colors.dart';
 import 'package:finhelper/src/shared/themes/app_text_styles.dart';
 import 'package:flutter/material.dart';
@@ -15,6 +14,11 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   SettingsController controller = SettingsController();
   final authController = AuthController();
+
+  getSettings() async {
+    return await controller.currentSettings();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,106 +32,126 @@ class _SettingsPageState extends State<SettingsPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 25),
-              child: Text(
-                'Geral',
-                style: AppTextStyles.titlePagesBlack,
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Notificações',
-                  style: AppTextStyles.subTitleBlack,
-                ),
-                Switch(
-                  value: controller.allowNotifications(),
-                  onChanged: (value) {
-                    setState(() {
-                      controller.setAllowNotifications(value);
-                    });
-                  },
-                  activeColor: AppColors.secondary,
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Lembrar login',
-                  style: AppTextStyles.subTitleBlack,
-                ),
-                Switch(
-                  value: controller.rememberLogin(),
-                  onChanged: (value) {
-                    setState(() {
-                      controller.setRememberLogin(value);
-                    });
-                  },
-                  activeColor: AppColors.secondary,
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Solicitar senha ao entrar',
-                  style: AppTextStyles.subTitleBlack,
-                ),
-                Switch(
-                  value: controller.allowPasswordOnLogin(),
-                  onChanged: (value) {
-                    setState(() {
-                      controller.setAllowPasswordOnLogin(value);
-                    });
-                  },
-                  activeColor: AppColors.secondary,
-                ),
-              ],
-            ),
-            ElevatedButton(
-              onPressed: () {
-                authController.clearUser(context);
-              },
-              child: Text(
-                'Sign out',
-                style: TextStyle(
-                  color: AppColors.whiteSoft,
-                ),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                await DBProvider.db.clearAll();
-              },
-              child: Text(
-                'Limpar Base - For Test',
-                style: TextStyle(
-                  color: AppColors.whiteSoft,
-                ),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                await DBProvider.db.createAll();
-              },
-              child: Text(
-                'Criar Base - For Test',
-                style: TextStyle(
-                  color: AppColors.whiteSoft,
-                ),
-              ),
-            ),
-          ],
-        ),
+        child: buildSettings(),
       ),
+    );
+  }
+
+  buildSettings() {
+    return FutureBuilder(
+        future: getSettings(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 25),
+                  child: Text(
+                    'Geral',
+                    style: AppTextStyles.titlePagesBlack,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Notificações',
+                      style: AppTextStyles.subTitleBlack,
+                    ),
+                    Switch(
+                      value: controller.allowNotifications(),
+                      onChanged: (value) {
+                        setState(() {
+                          controller.setAllowNotifications(value);
+                        });
+                      },
+                      activeColor: AppColors.secondary,
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Lembrar login',
+                      style: AppTextStyles.subTitleBlack,
+                    ),
+                    Switch(
+                      value: controller.rememberLogin(),
+                      onChanged: (value) {
+                        setState(() {
+                          controller.setRememberLogin(value);
+                        });
+                      },
+                      activeColor: AppColors.secondary,
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Solicitar senha ao entrar',
+                      style: AppTextStyles.subTitleBlack,
+                    ),
+                    Switch(
+                      value: controller.allowPasswordOnLogin(),
+                      onChanged: (value) {
+                        setState(() {
+                          controller.setAllowPasswordOnLogin(value);
+                        });
+                      },
+                      activeColor: AppColors.secondary,
+                    ),
+                  ],
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return alert();
+                        });
+                  },
+                  child: Text(
+                    'Encerrar conta',
+                    style: TextStyle(
+                      color: AppColors.whiteSoft,
+                    ),
+                  ),
+                ),
+              ],
+            );
+          } else {
+            return Container();
+          }
+        });
+  }
+
+  alert() {
+    return AlertDialog(
+      title: Text('Atenção'),
+      content: Text(
+          'Ao encerrar sua conta todos os seus dados serão deletados\nDeseja prosseguir? '),
+      actions: [
+        TextButton(
+          child: Text(
+            'Cancelar',
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        TextButton(
+            child: Text(
+              'Continuar',
+              style: TextStyle(color: Colors.black),
+            ),
+            onPressed: () {
+              authController.clearUser(context);
+            }),
+      ],
     );
   }
 }
