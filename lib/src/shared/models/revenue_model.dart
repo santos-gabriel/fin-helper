0059c60dcf1c1b1a767d3d7c47daa212c6cce6f7
gabriel <gabrielalmeidads@gmail.com>
2022-01-20 21:24:43 -0300

    Adding user id in revenue model

--- lib/src/shared/models/revenue_model.dart
+++ lib/src/shared/models/revenue_model.dart
@@ -1,47 +1,59 @@
 import 'dart:convert';
 
 class RevenueModel {
+  int? id;
   String? description;
   String? type;
   String? date;
   double? value;
+  int? userId;
 
   RevenueModel({
+    this.id,
     this.description,
     this.type,
     this.date,
     this.value,
+    this.userId,
   });
 
   RevenueModel copyWith({
+    int? id,
     String? description,
     String? type,
     String? date,
     double? value,
+    int? userId,
   }) {
     return RevenueModel(
+      id: id ?? this.id,
       description: description ?? this.description,
       type: type ?? this.type,
       date: date ?? this.date,
       value: value ?? this.value,
+      userId: userId ?? this.userId,
     );
   }
 
   Map<String, dynamic> toMap() {
     return {
+      'id': id,
       'description': description,
       'type': type,
       'date': date,
       'value': value,
+      'user_id': userId,
     };
   }
 
   factory RevenueModel.fromMap(Map<String, dynamic> map) {
     return RevenueModel(
+      id: map['ID'],
       description: map['DESCRIPTION'],
       type: map['TYPE'],
       date: map['DATE'],
       value: map['VALUE'],
+      userId: map['USER_ID'],
     );
   }
 
@@ -52,7 +64,7 @@ class RevenueModel {
 
   @override
   String toString() {
-    return 'RevenueModel(description: $description, type: $type, date: $date, value: $value)';
+    return 'RevenueModel(id: $id, description: $description, type: $type, date: $date, value: $value, userId: $userId)';
   }
 
   @override
@@ -60,17 +72,21 @@ class RevenueModel {
     if (identical(this, other)) return true;
 
     return other is RevenueModel &&
+        other.id == id &&
         other.description == description &&
         other.type == type &&
         other.date == date &&
-        other.value == value;
+        other.value == value &&
+        other.userId == userId;
   }
 
   @override
   int get hashCode {
-    return description.hashCode ^
+    return id.hashCode ^
+        description.hashCode ^
         type.hashCode ^
         date.hashCode ^
-        value.hashCode;
+        value.hashCode ^
+        userId.hashCode;
   }
 }
