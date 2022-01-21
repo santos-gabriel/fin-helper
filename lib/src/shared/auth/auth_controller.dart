1eb2903ed346bc059fea8c390135fe48f95a972f
gabriel <gabrielalmeidads@gmail.com>
2022-01-20 21:51:30 -0300

    Updating auth controller adding settings definition

--- lib/src/shared/auth/auth_controller.dart
+++ lib/src/shared/auth/auth_controller.dart
@@ -1,5 +1,8 @@
 import 'dart:io';
 
+import 'package:finhelper/src/modules/settings/settings_controller.dart';
+import 'package:finhelper/src/shared/database/DBProvider.dart';
+import 'package:finhelper/src/shared/models/settings_model.dart';
 import 'package:finhelper/src/shared/models/user_model.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:path_provider/path_provider.dart';
@@ -10,13 +13,18 @@ class AuthController {
 
   UserModel get user => _user!;
 
-  void setUser(BuildContext context, UserModel? user) {
+  void setUser(BuildContext context, UserModel? user,
+      {SettingsModel? settings}) {
     if (user != null) {
       saveUser(user);
       _user = user;
-      // Navigator.pushReplacementNamed(context, "/home", arguments: user);
-      Navigator.pushReplacementNamed(context, "/login-local-auth",
-          arguments: user);
+      if ((settings != null) &&
+          ((settings.allowPasswordOnLogin) || (!settings.rememberLogin))) {
+        Navigator.pushReplacementNamed(context, "/login-local-auth",
+            arguments: user);
+      } else {
+        Navigator.pushReplacementNamed(context, "/home", arguments: user);
+      }
     } else {
       Navigator.pushReplacementNamed(context, "/login");
     }
@@ -37,12 +45,19 @@ class AuthController {
     await instance.setString("user", user.toJson());
   }
 
-  Future<void> currentUser(BuildContext context) async {
+  Future<void> currentUser(BuildContext context, {bool? verifySettings}) async {
     final instance = await SharedPreferences.getInstance();
     await Future.delayed(Duration(seconds: 2));
     if (instance.containsKey("user")) {
       final json = instance.get("user") as String;
-      setUser(context, UserModel.fromJson(json));
+      if ((verifySettings != null) && (verifySettings == true)) {
+        final SettingsController settingsController = SettingsController();
+        await settingsController.currentSettings();
+        setUser(context, UserModel.fromJson(json),
+            settings: settingsController.settings);
+      } else {
+        setUser(context, UserModel.fromJson(json));
+      }
       return;
     } else {
       setUser(context, null);
@@ -57,8 +72,10 @@ class AuthController {
 
   Future<void> clearUser(BuildContext context) async {
     final instance = await SharedPreferences.getInstance();
+    await DBProvider.db.clearAll();
     await Future.delayed(Duration(seconds: 2));
     instance.remove("user");
+    instance.remove("settings");
     final Directory path = await getApplicationDocumentsDirectory();
     final String pathStr = path.path;
     final File file = File('$pathStr/user.png');
