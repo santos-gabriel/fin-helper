48492890d836fb1d5879b4bd1316a26d7f8cf95b
gabriel <gabrielalmeidads@gmail.com>
2022-01-20 21:21:20 -0300

    Adding id in user model

--- lib/src/shared/models/user_model.dart
+++ lib/src/shared/models/user_model.dart
@@ -1,14 +1,30 @@
 import 'dart:convert';
 
 class UserModel {
+  final int? id;
   final String name;
   final String? photoURL;
   final String? password;
 
-  UserModel({required this.name, this.photoURL, this.password});
+  UserModel({this.id, required this.name, this.photoURL, this.password});
+
+  UserModel copyWith({
+    int? id,
+    String? name,
+    String? photoURL,
+    String? password,
+  }) {
+    return UserModel(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      photoURL: photoURL ?? this.photoURL,
+      password: password ?? this.password,
+    );
+  }
 
   factory UserModel.fromMap(Map<String, dynamic> map) {
     return UserModel(
+      id: map['id'],
       name: map['name'],
       photoURL: map['photoURL'],
       password: map['password'],
@@ -19,6 +35,7 @@ class UserModel {
       UserModel.fromMap(jsonDecode(json));
 
   Map<String, dynamic> toMap() => {
+        "id": id,
         "name": name,
         "photoURL": photoURL,
         "password": password,
