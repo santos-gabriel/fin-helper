7b2bdaae790a8b2637c27a49c5c82d0799d5b355
gabriel <gabrielalmeidads@gmail.com>
2022-01-20 21:26:02 -0300

    Adding user id in expense model

--- lib/src/shared/models/expense_model.dart
+++ lib/src/shared/models/expense_model.dart
@@ -1,54 +1,65 @@
 import 'dart:convert';
 
 class ExpenseModel {
+  int? id;
   String? description;
   String? type;
   double? value;
   String? date;
   String? dueDate;
+  int? userId;
 
   ExpenseModel({
+    this.id,
     this.description,
     this.type,
     this.value,
     this.date,
     this.dueDate,
+    this.userId,
   });
 
   ExpenseModel copyWith({
+    int? id,
     String? description,
     String? type,
     double? value,
     String? date,
     String? dueDate,
+    int? userId,
   }) {
     return ExpenseModel(
+      id: id ?? this.id,
       description: description ?? this.description,
       type: type ?? this.type,
       value: value ?? this.value,
       date: date ?? this.date,
       dueDate: dueDate ?? this.dueDate,
+      userId: userId ?? this.userId,
     );
   }
 
   Map<String, dynamic> toMap() {
     return {
+      'id': id,
       'description': description,
       'type': type,
       'value': value,
       'date': date,
-      'dueDate': dueDate,
+      'due_date': dueDate,
+      'user_id': userId,
     };
   }
 
   factory ExpenseModel.fromMap(Map<String, dynamic> map) {
     return ExpenseModel(
-      description: map['DESCRIPTION'],
-      type: map['TYPE'],
-      value: map['VALUE'],
-      date: map['DATE'],
-      dueDate: map['DUEDATE'],
-    );
+        id: map['ID'],
+        description: map['DESCRIPTION'],
+        type: map['TYPE'],
+        value: map['VALUE'],
+        date: map['DATE'],
+        dueDate: map['DUE_DATE'],
+        userId: map['USER_ID']);
   }
 
   String toJson() => json.encode(toMap());
@@ -58,7 +69,7 @@ class ExpenseModel {
 
   @override
   String toString() {
-    return 'ExpenseModel(description: $description, type: $type, value: $value, date: $date, dueDate: $dueDate)';
+    return 'ExpenseModel(id: $id, description: $description, type: $type, value: $value, date: $date, dueDate: $dueDate, userId: $userId)';
   }
 
   @override
@@ -66,19 +77,23 @@ class ExpenseModel {
     if (identical(this, other)) return true;
 
     return other is ExpenseModel &&
+        other.id == id &&
         other.description == description &&
         other.type == type &&
         other.value == value &&
         other.date == date &&
-        other.dueDate == dueDate;
+        other.dueDate == dueDate &&
+        other.userId == userId;
   }
 
   @override
   int get hashCode {
-    return description.hashCode ^
+    return id.hashCode ^
+        description.hashCode ^
         type.hashCode ^
         value.hashCode ^
         date.hashCode ^
-        dueDate.hashCode;
+        dueDate.hashCode ^
+        userId.hashCode;
   }
 }
