59abcfce734c5f60b175b217cdc4f080d7214b0a
gabriel <gabrielalmeidads@gmail.com>
2022-02-01 22:10:01 -0300

    Adding empty balance image

--- lib/src/shared/themes/app_images.dart
+++ lib/src/shared/themes/app_images.dart
@@ -2,4 +2,5 @@ class AppImages {
   static const logoFull = "assets/images/logofull.png";
   static const logoMini = "assets/images/logomini.png";
   static const google = "assets/images/google.png";
+  static const emptyBalance = "assets/images/emptybalance.png";
 }
