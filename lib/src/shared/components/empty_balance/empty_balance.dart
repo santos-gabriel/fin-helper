94b0bec29119678d5d6f0d2c923af8b334f780f5
gabriel <gabrielalmeidads@gmail.com>
2022-03-23 23:07:11 -0300

    Adding delayed on empty balance

--- lib/src/shared/components/empty_balance/empty_balance.dart
+++ lib/src/shared/components/empty_balance/empty_balance.dart
@@ -1,28 +1,71 @@
-import 'package:finhelper/src/shared/themes/app_images.dart';
-import 'package:finhelper/src/shared/themes/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
-class EmptyBalance extends StatelessWidget {
-  const EmptyBalance({Key? key}) : super(key: key);
+import 'package:finhelper/src/shared/themes/app_images.dart';
+import 'package:finhelper/src/shared/themes/app_text_styles.dart';
+
+class EmptyBalance extends StatefulWidget {
+  final int? secondsDelayed;
+  const EmptyBalance({
+    Key? key,
+    this.secondsDelayed,
+  }) : super(key: key);
+
+  @override
+  State<EmptyBalance> createState() => _EmptyBalanceState();
+}
+
+class _EmptyBalanceState extends State<EmptyBalance> {
+  getDelay() async {
+    await Future.delayed(Duration(seconds: widget.secondsDelayed!));
+    return true;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Image.asset(AppImages.emptyBalance),
-          Divider(
-            thickness: 1,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 2),
-            child: Text(
-              'Nada por aqui...',
-              style: AppTextStyles.titleEmptyBalance,
+    if (widget.secondsDelayed != null) {
+      return FutureBuilder(
+          future: getDelay(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Container(
+                child: Column(
+                  children: [
+                    Image.asset(AppImages.emptyBalance),
+                    Divider(
+                      thickness: 1,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 2),
+                      child: Text(
+                        'Nada por aqui...',
+                        style: AppTextStyles.titleEmptyBalance,
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            } else {
+              return Container();
+            }
+          });
+    } else {
+      return Container(
+        child: Column(
+          children: [
+            Image.asset(AppImages.emptyBalance),
+            Divider(
+              thickness: 1,
             ),
-          ),
-        ],
-      ),
-    );
+            Padding(
+              padding: const EdgeInsets.only(top: 2),
+              child: Text(
+                'Nada por aqui...',
+                style: AppTextStyles.titleEmptyBalance,
+              ),
+            ),
+          ],
+        ),
+      );
+    }
   }
 }
